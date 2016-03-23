0d41d27889fb0b5dae4245925359a5073913fa4d
bijoy <bijoysingh693@gmail.com>
2016-03-23 21:08:51 +0530

    Fixing url

--- order/urls.py
+++ order/urls.py
@@ -4,6 +4,6 @@ from order import views
 urlpatterns = patterns(
     '',
     url(r'^make_order/(\w+)$', views.make_order, name='make_order'),
-    url(r'^add_item/(\w+)/(\d+)$', views.add_item, name='add_item'),
+    url(r'^add_item/(\w+)/(\w+)$', views.add_item, name='add_item'),
     url(r'^verify_weight/(\w+)/(\d+)', views.verify_weight, name='verify_weight'),
 )
