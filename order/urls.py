248c6ba5eabfacbfaf9aa541ecd288938d250693
bijoy <bijoysingh693@gmail.com>
2016-04-04 23:08:40 +0530

    float issue

--- order/urls.py
+++ order/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns(
     '',
     url(r'^make_order/(\w+)$', views.make_order, name='make_order'),
     url(r'^add_item/(\w+)/(\w+)$', views.add_item, name='add_item'),
-    url(r'^verify_weight/(\w+)/(\d+)$', views.verify_weight, name='verify_weight'),
+    url(r'^verify_weight/(\w+)/([-+]?\d*\.\d+|\d+)$', views.verify_weight, name='verify_weight'),
 )
